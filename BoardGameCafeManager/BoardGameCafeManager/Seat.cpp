599d5bdb96e3007a79152dfe8e8bbf4e016a601d
qhtkdrma <38090115+qhtkdrma@users.noreply.github.com>
2018-12-25 12:42:18 +0900

    파일 입출력 완료 자리 입력 완료(주석은 추후 바꿀예정)

--- BoardGameCafeManager/BoardGameCafeManager/Seat.cpp
+++ BoardGameCafeManager/BoardGameCafeManager/Seat.cpp
@@ -4,7 +4,7 @@
 #include "Seat.h"
 #include <iostream>
 #include <string>
-
+#include <fstream>
 
 //Setter Function
 void Seat::setGame(std::string game) { this->game = game; };
@@ -21,26 +21,58 @@ std::string Seat::getName() { return name; };
 Seat* Seat::getNext() { return next; };
 
 //기능 함수
-void Seat::SeatAdd(std::string line, std::string name, int number, std::string g)
+void Seat::SeatLine(std::string line)
 {
 	Seat *seat = new Seat;
 
-	if (head == NULL)  //head,last pointer setting
+	if (this->head == NULL)  //head,last pointer setting
 	{
-		head = seat;
-		last = seat;
+		this->head = seat;
+		this->last = seat;
 	}
 	else   //pointing next seat
 	{
-		last->setNext(seat);
-		last = seat;
+		this->last->setNext(seat);
+		this->last = seat;
 	}
 
-	seat->setLine(line); seat->setName(name), seat->setNumber(number), seat->setGame(g);//각 변수의 저장
+	seat->setLine(line);
+};
+void Seat::SeatAdd(std::string line, std::string name, int number, std::string g)
+{
+	Seat *seat = NULL;
+	bool check = false;
+
+	while (true)
+	{
+		if (seat == NULL) // seat초기값 설정
+			seat = head;
+		if (line == seat->getLine())
+		{
+			seat->setName(name), seat->setNumber(number), seat->setGame(g);//각 변수의 저장
+			check = true;
+		}
+		if (seat->getNext() == NULL)// 마지막 노드에 도달하면 함수 종료
+		{
+			if (check = false)
+			{
+				std::cout << "예약된 자리가 없습니다.\n";
+				return;
+			}
+			else
+			{
+				std::cout << "예약되었습니다.\n";
+				return;
+			}
+		}
+
+		seat = seat->getNext();
+	}
 };
 void Seat::SeatList()
 {
 	Seat *seat = NULL;
+	int cnt = 0;
 
 	while (true)
 	{
@@ -51,10 +83,22 @@ void Seat::SeatList()
 		}
 		if (seat == NULL) // seat초기값 설정
 			seat = head;
-		std::cout << "자리 : " << seat->getLine() << "\t이름 : " << seat->getName() //예약된 자리, 이름, 인원수, 할 게임 리스트로 출력
-			<< "\t인원수 : " << seat->getNumber() << "\t게임 : " << seat->getGame() << std::endl;
+		if (seat->getNumber() != 0)
+		{
+			std::cout << "자리 : " << seat->getLine() << "\t이름 : " << seat->getName() //예약된 자리, 이름, 인원수, 할 게임 리스트로 출력
+				<< "\t인원수 : " << seat->getNumber() << "\t게임 : " << seat->getGame() << std::endl;
+			cnt++;
+		}
 		if (seat->getNext() == NULL)// 마지막 노드에 도달하면 함수 종료
-			return;
+		{
+			if (cnt == 0)
+			{
+				std::cout << "예약된 자리가 없습니다.\n";
+				return;
+			}
+			else
+				return;
+		}
 
 		seat = seat->getNext();
 	}
@@ -69,11 +113,6 @@ void Seat::SeatSearch(std::string searchStr, std::string mod)
 	Seat *seat = NULL;
 	while (true)
 	{
-		if (head == NULL || searchStr != (head->*func)())//리스트에 아무것도 없을 때 또는 잘못된 값을 넣었을때 나타나는 오류 방지
-		{
-			std::cout << "예약 인원이 없습니다.\n";
-			return;
-		}
 		if (seat == NULL) // seat초기값 설정
 			seat = head;
 		if (searchStr == (seat->*func)())//
@@ -104,8 +143,8 @@ void Seat::SeatDelete(std::string searchStr, std::string mod)
 		{
 			lastIndex->setNext(firstIndex->getNext());
 			delete firstIndex;
+			firstIndex = NULL;
 			std::cout << "삭제가 완료되었습니다.\n";
-			this->head = NULL;//헤더에 쓰레기값 방지
 			return;
 		}
 		if (firstIndex->getNext() == NULL)// 마지막 노드에 도달하면 함수 종료
@@ -122,4 +161,33 @@ void Seat::Showmenu()
 	std::cout << "3. 자리 예약 검색\n";
 	std::cout << "4. 자리 예약 삭제\n";
 	std::cout << "0. 돌아가기\n";
+};
+void Seat::SeatSave(int width, int length)
+{
+	std::fstream fileStream;
+	fileStream.open("reservation.txt", std::ios::out);
+	// 고객 정보를 파일에 저장(매번 파일로드후 저장함)
+	for (int i = 0; i < width; i++)
+	{
+		for (int j = 0; j < length; j++)
+		{
+			fileStream << i + 1 << "-" << j + 1 << std::endl;//파일에 자리 입력
+		}
+	}
+	fileStream.close();
+}
+void Seat::SeatLoad(int width, int length)
+{
+	std::fstream fileStream;
+	fileStream.open("reservation.txt", std::ios::in);
+	if (!fileStream.is_open())//파일이 없으면 경고
+		return;
+	std::string line;
+	//파일 로드(매번 로드해서 링크드리스트에 저장함)
+	for (int i = 0; i < width * length; i++)
+	{
+		fileStream >> line;//wid, len을 모두 string으로 받아서 라인에 입력
+		SeatLine(line);
+	}
+	fileStream.close();
 };
